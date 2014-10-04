71f424df3adb0bdec4f54a949dfa5a0be2900971
Caleb James DeLisle <cjd@cjdns.fr>
2014-10-04 21:31:13 +0200

    Toshitba chip needs to use .driver_info=AR9280_USB

--- ath9k/hif_usb.c
+++ ath9k/hif_usb.c
@@ -56,8 +56,8 @@ static struct usb_device_id ath9k_hif_usb_ids[] = {
 	  .driver_info = AR9280_USB },  /* Sony UWA-BR100 */
 	{ USB_DEVICE(0x04da, 0x3904),
 	  .driver_info = AR9280_USB },
-
-  { USB_DEVICE(0x0930, 0x0A08) }, /* Toshiba WLM 20U2 */
+	{ USB_DEVICE(0x0930, 0x0A08),
+	  .driver_info = AR9280_USB },  /* Toshiba WLM 20U2 */
 
 	{ USB_DEVICE(0x0cf3, 0x20ff),
 	  .driver_info = STORAGE_DEVICE },
